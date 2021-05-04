7b23b2453b18ac34ea064836f04cc53fcb6fcdb9
Greyson Mainor <gbmainor98@outlook.com>
2021-05-04 15:48:15 -0400

    attempting to fix my files by replacing them with clones of themseleves.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import "../src/";
+import ".App.css";
 import Main from './components/Main';
 import Wrapper from './components/Wrapper';
 import Header from './components/Header';
